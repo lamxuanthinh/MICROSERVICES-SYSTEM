66e85cb6965785e04377e0b17e89866ce08ddcba
vietthuan03 <thuantv.21it@vku.udn.vn>
2023-10-20 15:19:34 +0700

    Cart

---
+++ Carts/src/main/java/com/example/mywebapp/user/UserMessage.java
@@ -0,0 +1,6 @@
+package com.example.mywebapp.user;
+
+public class UserMessage {
+    public static final String USER_SAVED = "The user has been saved successfully.";
+    public static final String USER_EDIT = "The user has been EDIT successfully.";
+}
---
+++ Carts/src/main/java/com/example/mywebapp/user/UserService.java
@@ -0,0 +1,40 @@
+package com.example.mywebapp.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> listAll() {
+        return (List<User>) repo.findAll();
+    }
+
+    public void save(User user) {
+        repo.save(user);
+    }
+    public User get(Integer id) throws UserNotFoundException {
+        Optional<User> result = repo.findById(id);
+        if(result.isPresent()){
+            return result.get();
+        }
+        throw new UserNotFoundException("Could not find any users with ID " + id);
+    }
+    public void delete(Integer id) throws UserNotFoundException {
+        Long count = repo.countById(id);
+        if (count == null || count == 0) {
+            throw new UserNotFoundException("Could not find any users with ID " + id);
+        }
+        repo.deleteById(id);
+    }
+
+}
