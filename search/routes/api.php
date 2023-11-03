6702dc19ac3fefc6c1461041d4fdf4c0b5a52320
Duy Tính <117659616+DTinh@users.noreply.github.com>
2023-11-03 13:53:52 +0700

    Update api.php

--- search/routes/api.php
+++ search/routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
+|http://localhost:8080
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
